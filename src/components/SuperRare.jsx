4adec12628163b1f78e0d1f764a91713f7c56e4e
Dima Odintsov <dimaodin1@icloud.com>
2022-03-13 01:10:54 +0200

    #2

--- src/components/SuperRare.jsx
+++ src/components/SuperRare.jsx
@@ -46,7 +46,7 @@ export default function SuperRare() {
         <h2 className="title">LE Super Rare Auction</h2>
         <p className="description">
           We have released four limited edition NFT's early which which can be
-          bid on via <a href="#">OpenSea</a>.
+          bid on via <a href="https://opensea.io/">OpenSea</a>.
         </p>
       </div>
       <div className="cards">
